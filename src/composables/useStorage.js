2f03f4327a41c5ab71655be5d91d7b0004611ef6
Fred Spooner <fred@aghq.uk>
2023-03-07 10:08:00 +0000

    improvements to support (de-)serialization

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -1,8 +1,21 @@
 import { ref, watch } from "vue";
 
 export function useStorage(key, defaultVal = null) {
+    const write = () => {
+        if (val.value === '') {
+            removeItem();
+        } else {
+            localStorage.setItem(key, JSON.stringify(val.value));
+        }
+    }
+
+    const read = () => {
+        return JSON.parse(localStorage.getItem(key));
+    }
+
     let val;
-    let storedValue = localStorage.getItem(key);
+    let storedValue = read();
+
 
     if (storedValue){
         val = ref(storedValue);
@@ -13,17 +26,10 @@ export function useStorage(key, defaultVal = null) {
 
     write();
 
-    function write() {
-        if (val.value === '') {
-            removeItem();
-        } else {
-            localStorage.setItem(key, val.value);
-        }
-    }
 
     const removeItem = () => localStorage.removeItem(key);
 
-    watch(val, () => write());
+    watch(val, write);
 
     return val;
 }
